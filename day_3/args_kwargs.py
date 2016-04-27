0b34c6c6a998b782fdd5796b02d91be4acfe338a
Cate <crakama89@gmail.com>
2016-04-27 14:38:48 +0300

    day 3: complete args/kwargs

--- day_3/args_kwargs.py
+++ day_3/args_kwargs.py
@@ -24,6 +24,64 @@ for person in people:
     print hello(*person)
 
 
+def super_sum(a, b, *args):
+    '''
+    Takes in variable number of items,
+    and returns the sum.
+    e.g. super_sum(10, 20) = 30
+        super_sum(10, 20, 40) = 70
+        super_sum(1, 4, 5, 6, 7) = 23
+    '''
+    total = 0
+    for i in args:
+        total += i
+
+    return total + a + b
+
+print super_sum(10, 20)
+print super_sum(1, 4, 5, 7)
+a = [10, 40, 60]
+print super_sum(*a)
+print super_sum(10, 20)
+
+def hello_again(**kwargs):
+    return "I am {}, and I'm {}".format(kwargs['name'], kwargs['age'])
+
+
+print hello_again(name='Joe', age=20)
+print hello_again(age=20, name='Jane')
+
+other_people = [
+        {'name': 'Joe', 'age': 98},
+        {'name': 'Jane', 'age': 60},
+        {'name': 'Trump', 'age': 23}
+    ]
+
+joe = {'name': 'Joe', 'age': 98}
+
+print hello_again(**joe)
+print hello_again(name='Joe', age=98)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
